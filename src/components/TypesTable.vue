389e95398578fc8addbf6c69ee3f203384527aa2
Marcelo Martínez <marcelo2m1097@gmail.com>
2021-06-18 08:36:11 -0600

    :art: Modularize components structure

--- src/components/TypesTable.vue
+++ src/components/TypesTable.vue
@@ -3,151 +3,51 @@
     <table>
       <thead>
         <tr>
-          <th v-for="(header, index) in tableHeaders" :key="index" >{{ header }}</th>
+          <typ-lne-header></typ-lne-header>
         </tr>
       </thead>
       <tbody>
-        <tr v-for="(row, index) in tableContent" :key="index">
-          <td v-for="(column, index) of row" :key="index" @click="openModal()">
-            {{ column }}
-          </td>
-        </tr>
+        <typ-lne-content @openModal="openModal"></typ-lne-content>
       </tbody>
     </table>
     <!-- TODO: Add Type Form -->
     <div>
-      <transition name="fade">
-        <div class="modal" v-if="isModalOpen">
-          <div class="modal__backdrop" @click="openModal()"/>
-          <div class="modal__dialog">
-            <div class="modal__header">
-              <button type="button" class="modal__close" @click="openModal()">
-                <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 352 512">
-                  <path
-                    fill="currentColor"
-                    d="M242.72 256l100.07-100.07c12.28-12.28 12.28-32.19 0-44.48l
-                    -22.24-22.24c-12.28-12.28-32.19-12.28-44.48 0L176 189.28 75.93
-                    89.21c-12.28-12.28-32.19-12.28-44.48 0L9.21 111.45c-12.28 12.28
-                    -12.28 32.19 0 44.48L109.28 256 9.21 356.07c-12.28 12.28-12.28 32.19
-                    0 44.48l22.24 22.24c12.28 12.28 32.2 12.28 44.48 0L176 322.72l100.07
-                    100.07c12.28 12.28 32.2 12.28 44.48 0l22.24-22.24c12.28-12.28 12.28-32.19
-                    0-44.48L242.72 256z"
-                  ></path>
-                </svg>
-              </button>
-            </div>
-
-            <div class="modal__body">
-              <slot name="body"/>
-            </div>
-
-            <div class="modal__footer">
-              <slot name="footer"/>
-            </div>
-
-          </div>
-        </div>
-      </transition>
+      <typ-lne-modal v-if="isModalOpen" @closeModal="closeModal"></typ-lne-modal>
     </div>
   </div>
 </template>
 
 <script lang="ts">
+import TypLneHeader from './grid-headers/TypLneHeader.vue';
+import TypLneContent from './grid-content/TypLneContent.vue';
+import TypLneModal from './modals/TypLneModal.vue';
+
 export default {
-  // Missing return type
+  components: {
+    TypLneHeader,
+    TypLneContent,
+    TypLneModal,
+  },
+
+  // TODO: Missing return type
   data() {
-    // TODO: Get headers from backend
-    const tableHeaders: string[] = [
-      'ID(a:40)',
-      'loc_name(a:40)',
-    ];
-    // TODO: Get content from backend
-    // TODO: Create type for each table
-    const tableContent = [
-      {
-        id: 1,
-        loc_name: 'Element name',
-      },
-      {
-        id: 2,
-        loc_name: 'Another element name',
-      },
-    ];
     const isModalOpen = false;
     return {
-      tableHeaders,
-      tableContent,
       isModalOpen,
     };
   },
 
   methods: {
     openModal(): void {
-      this.isModalOpen = !this.isModalOpen;
+      this.isModalOpen = true;
+    },
+    closeModal(): void {
+      this.isModalOpen = false;
     },
   },
 };
 </script>
 
 <style lang="scss" scoped>
-.modal {
-  overflow-x: hidden;
-  overflow-y: auto;
-  position: fixed;
-  top: 0;
-  right: 0;
-  bottom: 0;
-  left: 0;
-  z-index: 9;
-  &__backdrop {
-    background-color: rgba(0, 0, 0, 0.3);
-    position: fixed;
-    top: 0;
-    right: 0;
-    bottom: 0;
-    left: 0;
-    z-index: 1;
-  }
-  &__dialog {
-    background-color: #ffffff;
-    position: relative;
-    width: 600px;
-    margin: 50px auto;
-    display: flex;
-    flex-direction: column;
-    border-radius: 5px;
-    z-index: 2;
-    @media screen and (max-width: 992px) {
-      width: 90%;
-    }
-  }
-  &__close {
-    width: 30px;
-    height: 30px;
-  }
-  &__header {
-    padding: 20px 20px 10px;
-    display: flex;
-    align-items: flex-start;
-    justify-content: space-between;
-  }
-  &__body {
-    padding: 10px 20px 10px;
-    overflow: auto;
-    display: flex;
-    flex-direction: column;
-    align-items: stretch;
-  }
-  &__footer {
-    padding: 10px 20px 20px;
-  }
-}
-.fade-enter-active,
-.fade-leave-active {
-  transition: opacity 0.2s;
-}
-.fade-enter,
-.fade-leave-to {
-  opacity: 0;
-}
+
 </style>
---
+++ src/components/grid-content/TypLneContent.vue
@@ -0,0 +1,37 @@
+<template>
+  <tr v-for="(row, index) in tableContent" :key="index">
+    <td v-for="(column, index) of row" :key="index" @click="$emit('openModal')">
+      {{ column }}
+    </td>
+  </tr>
+</template>
+
+<script lang="ts">
+export default {
+  template: 'typlnecontent',
+
+  // TODO: Missing return type
+  data() {
+    // TODO: Get content from backend
+    // TODO: Create type for each table
+    const tableContent = [
+      {
+        id: 1,
+        loc_name: 'Element name',
+      },
+      {
+        id: 2,
+        loc_name: 'Another element name',
+      },
+    ];
+
+    return {
+      tableContent,
+    };
+  },
+};
+</script>
+
+<style lang="scss" scoped>
+
+</style>
