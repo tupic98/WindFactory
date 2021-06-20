506cef6d8fab26ab75864c3d0e76d5cdf923c912
Rodrigo Alvarenga <rodrigo.alvarenga@fituapp.com>
2021-06-19 18:56:24 -0600

    :art: Added typswitch form component

---
+++ src/components/TypesForm/TypSwitch.vue
@@ -0,0 +1,190 @@
+<template>
+  <div>
+    <!-- Tabs -->
+    <div>
+      <div class="sm:hidden">
+        <label for="tabs" class="sr-only">Select a tab</label>
+        <select id="tabs" name="tabs" class="block w-full pl-3 pr-10 py-2 text-base border-gray-300 focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm rounded-md" @change.prevent="context.selectedTab = $event.target.value">
+          <option value="general">General</option>
+          <option value="load-flow">Load Flow</option>
+        </select>
+      </div>
+      <div class="hidden sm:block">
+        <div class="border-b border-gray-200">
+          <nav class="-mb-px flex space-x-8" aria-label="Tabs">
+            <!-- Current: "border-indigo-500 text-indigo-600", Default: "border-transparent text-gray-500 hover:text-gray-700 hover:border-gray-300" -->
+            <div class="border-transparent text-gray-500 hover:text-gray-700 hover:border-gray-300 whitespace-nowrap py-4 px-1 border-b-2 font-medium text-sm cursor-pointer transition duration-300 ease-in-out" :class="{ 'border-indigo-500 text-indigo-600 hover:border-indigo-500 hover:text-indigo-600': context.selectedTab === 'general' }" @click="context.selectedTab = 'general'">
+              General
+            </div>
+
+            <div class="border-transparent text-gray-500 hover:text-gray-700 hover:border-gray-300 whitespace-nowrap py-4 px-1 border-b-2 font-medium text-sm cursor-pointer transition duration-300 ease-in-out" :class="{ 'border-indigo-500 text-indigo-600 hover:border-indigo-500 hover:text-indigo-600': context.selectedTab === 'load-flow' }" @click="context.selectedTab = 'load-flow'">
+              Load Flow
+            </div>
+          </nav>
+        </div>
+      </div>
+    </div>
+    <form>
+      <div v-show="context.selectedTab === 'general'" class="pt-6 text-left">
+        <div class="space-y-8 divide-y divide-gray-200">
+          <div class="space-y-8 divide-y divide-gray-200 sm:space-y-5">
+            <div class="pt-8 space-y-6 sm:pt-10 sm:space-y-5">
+              <div>
+                <h3 class="text-lg leading-6 font-medium text-gray-900">
+                  General
+                </h3>
+                <!-- <p class="mt-1 max-w-2xl text-sm text-gray-500">
+                  Use a permanent address where you can receive mail.
+                </p> -->
+              </div>
+              <div class="space-y-6 md:space-y-5">
+                <div class="md:grid md:grid-cols-4 md:gap-4 md:items-start md:border-t md:border-gray-200 md:pt-5">
+                  <div class="md:col-span-2 md:grid md:grid-cols-3 md:gap-4 md:items-start">
+                    <label for="loc_name" class="block text-sm font-medium text-gray-700 md:mt-px md:pt-2">
+                      Name
+                    </label>
+                    <div class="mt-1 sm:mt-0 sm:col-span-2">
+                      <input v-model="context.form.loc_name" type="text" name="loc_name" id="loc_name" class="max-w-lg block w-full shadow-sm focus:ring-indigo-500 focus:border-indigo-500 sm:max-w-xs sm:text-sm border-gray-300 rounded-md">
+                    </div>
+                  </div>
+                </div>
+              </div>
+              <div class="space-y-6 md:space-y-5">
+                <div class="md:grid md:grid-cols-4 md:gap-4 md:items-start md:border-t md:border-gray-200 md:pt-5">
+                  <div class="md:col-span-2 md:grid md:grid-cols-3 md:gap-4 md:items-start">
+                    <label for="Inom" class="block text-sm font-medium text-gray-700 md:mt-px md:pt-2">
+                      Rated Current
+                    </label>
+                    <div class="mt-1 sm:mt-0 sm:col-span-2">
+                      <div class="mt-1 relative rounded-md shadow-sm max-w-xs">
+                        <input v-model="context.form.Inom" type="text" name="Inom" id="Inom" class="max-w-lg block w-full shadow-sm focus:ring-indigo-500 focus:border-indigo-500 sm:max-w-xs sm:text-sm border-gray-300 rounded-md">
+                        <div class="absolute inset-y-0 right-0 pr-3 flex items-center pointer-events-none">
+                          <span class="text-gray-500 sm:text-sm">
+                            kA
+                          </span>
+                        </div>
+                      </div>
+                    </div>
+                  </div>
+                </div>
+              </div>
+              <div class="space-y-6 md:space-y-5">
+                <div class="md:grid md:grid-cols-4 md:gap-4 md:items-start md:border-t md:border-gray-200 md:pt-5">
+                  <div class="md:col-span-2 md:grid md:grid-cols-3 md:gap-4 md:items-start">
+                    <label for="t_open" class="block text-sm font-medium text-gray-700 md:mt-px md:pt-2">
+                      Breaker Opening Time
+                    </label>
+                    <div class="mt-1 sm:mt-0 sm:col-span-2 relative">
+                      <div class="mt-1 relative rounded-md shadow-sm max-w-xs">
+                        <input v-model="context.form.t_open" type="text" name="t_open" id="t_open" class="max-w-lg block w-full shadow-sm focus:ring-indigo-500 focus:border-indigo-500 sm:max-w-xs sm:text-sm border-gray-300 rounded-md">
+                        <div class="absolute inset-y-0 right-0 pr-3 flex items-center pointer-events-none">
+                          <span class="text-gray-500 sm:text-sm">
+                            s
+                          </span>
+                        </div>
+                      </div>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div v-show="context.selectedTab === 'load-flow'" class="pt-6 text-left">
+        <div class="space-y-8 divide-y divide-gray-200">
+          <div class="space-y-8 divide-y divide-gray-200 sm:space-y-5">
+            <div class="pt-8 space-y-6 sm:pt-10 sm:space-y-5">
+              <div>
+                <h3 class="text-lg leading-6 font-medium text-gray-900">
+                  Load Flow
+                </h3>
+                <!-- <p class="mt-1 max-w-2xl text-sm text-gray-500">
+                  Use a permanent address where you can receive mail.
+                </p> -->
+              </div>
+              <div class="space-y-6 md:space-y-5">
+                <div class="md:grid md:grid-cols-4 md:gap-4 md:items-start md:border-t md:border-gray-200 md:pt-5">
+                  <div class="md:col-span-2 md:grid md:grid-cols-3 md:gap-4 md:items-start">
+                    <label for="R_on" class="block text-sm font-medium text-gray-700 md:mt-px md:pt-2">
+                      On - Resistance
+                    </label>
+                    <div class="mt-1 sm:mt-0 sm:col-span-2">
+                      <div class="mt-1 relative rounded-md shadow-sm max-w-xs">
+                        <input v-model="context.form.R_on" type="text" name="R_on" id="R_on" class="max-w-lg block w-full shadow-sm focus:ring-indigo-500 focus:border-indigo-500 sm:max-w-xs sm:text-sm border-gray-300 rounded-md">
+                        <div class="absolute inset-y-0 right-0 pr-3 flex items-center pointer-events-none">
+                          <span class="text-gray-500 sm:text-sm">
+                            Ohm
+                          </span>
+                        </div>
+                      </div>
+                    </div>
+                  </div>
+                </div>
+              </div>
+              <div class="space-y-6 md:space-y-5">
+                <div class="md:grid md:grid-cols-4 md:gap-4 md:items-start md:border-t md:border-gray-200 md:pt-5">
+                  <div class="md:col-span-2 md:grid md:grid-cols-3 md:gap-4 md:items-start">
+                    <label for="X_on" class="block text-sm font-medium text-gray-700 md:mt-px md:pt-2">
+                      On - Reactance
+                    </label>
+                    <div class="mt-1 sm:mt-0 sm:col-span-2 relative">
+                      <div class="mt-1 relative rounded-md shadow-sm max-w-xs">
+                        <input v-model="context.form.X_on" type="text" name="X_on" id="X_on" class="max-w-lg block w-full shadow-sm focus:ring-indigo-500 focus:border-indigo-500 sm:max-w-xs sm:text-sm border-gray-300 rounded-md">
+                        <div class="absolute inset-y-0 right-0 pr-3 flex items-center pointer-events-none">
+                          <span class="text-gray-500 sm:text-sm">
+                            Ohm
+                          </span>
+                        </div>
+                      </div>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div class="pt-5">
+        <div class="flex justify-end">
+          <button type="button" class="bg-white py-2 px-4 border border-gray-300 rounded-md shadow-sm text-sm font-medium text-gray-700 hover:bg-gray-50 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500">
+            Cancel
+          </button>
+          <button type="submit" class="ml-3 inline-flex justify-center py-2 px-4 border border-transparent shadow-sm text-sm font-medium rounded-md text-white bg-indigo-600 hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500">
+            Save
+          </button>
+        </div>
+      </div>
+    </form>
+  </div>
+</template>
+<script lang="ts">
+import { Options, Vue, setup } from 'vue-class-component';
+import { toRefs, reactive } from 'vue';
+
+interface State {
+  selectedTab: string;
+}
+
+const useContext = () => {
+  const state: State = reactive({
+    selectedTab: 'general',
+    form: {
+      loc_name: '',
+      Inom: '',
+      t_open: '',
+      R_on: '',
+      X_on: '',
+    },
+  });
+
+  return {
+    ...toRefs(state),
+  };
+};
+
+@Options({})
+export default class TypSwitch extends Vue {
+  context = setup(() => useContext())
+}
+</script>
