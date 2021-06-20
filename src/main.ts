f0c010ee87ca935e79e760efdec2fcb8e0824a96
Rodrigo Alvarenga <rodrigo.alvarenga@fituapp.com>
2021-06-19 19:27:05 -0600

    :package: Add click outside dependency

--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,9 @@
 import { createApp } from 'vue';
+import VueClickAway from 'vue3-click-away';
 import App from './App.vue';
 import router from './router';
 import store from './store';
 import './index.scss';
 
-createApp(App).use(store).use(router).mount('#app');
+createApp(App).use(store).use(router).use(VueClickAway)
+  .mount('#app');
