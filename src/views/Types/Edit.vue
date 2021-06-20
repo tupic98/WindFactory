ecd37d5bdf009bcf784482ceb2edbf71cb3bfb2f
Rodrigo Alvarenga <rodrigo.alvarenga@fituapp.com>
2021-06-19 18:56:34 -0600

    :art: Added types view

---
+++ src/views/Types/Edit.vue
@@ -0,0 +1,17 @@
+<template>
+  <div>
+    <types-form />
+  </div>
+</template>
+
+<script lang="ts">
+import { Options, Vue } from 'vue-class-component';
+import TypesForm from '@/components/TypesForm/Index.vue';
+
+@Options({
+  components: {
+    TypesForm,
+  },
+})
+export default class TypesEditView extends Vue {}
+</script>
---
+++ src/views/Types/Index.vue
@@ -0,0 +1,17 @@
+<template>
+  <div>
+    <types />
+  </div>
+</template>
+
+<script lang="ts">
+import { Options, Vue } from 'vue-class-component';
+import Types from '@/components/Types/Index.vue';
+
+@Options({
+  components: {
+    Types,
+  },
+})
+export default class TypesView extends Vue {}
+</script>
