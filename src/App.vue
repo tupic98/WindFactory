962e515bac01428109d7b53fd5a3b07969a2531a
Rodrigo Alvarenga <rodrigo.alvarenga@fituapp.com>
2021-06-19 18:54:33 -0600

    :sparkles: Update base views

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,23 @@
 <template>
-  <router-view/>
+  <div class="min-h-screen flex h-full">
+    <navbar class="min-w-min w-64"/>
+    <div class="min-h-screen h-full w-10/12">
+      <router-view/>
+    </div>
+  </div>
 </template>
+
+<script lang="ts">
+import { Options, Vue } from 'vue-class-component';
+import Navbar from '@/components/Navbar.vue';
+
+@Options({
+  components: {
+    Navbar,
+  },
+})
+export default class App extends Vue {}
+</script>
 
 <style lang="scss">
 #app {
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,17 +1,12 @@
 <template>
   <div>
-    <types-table/>
   </div>
 </template>
 
 <script lang="ts">
 import { Options, Vue } from 'vue-class-component';
-import TypesGrid from '@/components/TypesGrid.vue'; // @ is an alias to /src
 
 @Options({
-  components: {
-    TypesGrid,
-  },
 })
-export default class Home extends Vue {}
+export default class HomeView extends Vue {}
 </script>
